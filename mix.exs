fb95a76381656cc32cf339d0c0d91276de33afb6
Michael Truog <mjtruog@gmail.com>
2014-10-30 15:12:16 -0700

    Tweak hex package.

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule ReltoolUtil.Mixfile do
   def project do
     [app: :reltool_util,
      version: "1.4.0-rc.4",
+     language: :erlang,
      description: description,
      package: package,
      deps: deps]
