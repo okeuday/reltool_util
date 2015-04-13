7307aee5a9ab071fd2eb4f20f5452e4caf35d275
Michael Truog <mjtruog@gmail.com>
2015-04-12 17:20:54 -0700

    Version 1.5.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ReltoolUtil.Mixfile do
 
   def project do
     [app: :reltool_util,
-     version: "1.4.0",
+     version: "1.5.0",
      language: :erlang,
      description: description,
      package: package,
