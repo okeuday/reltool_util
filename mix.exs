42e1d024e2f5fab5f39138bac8866213256679a7
Michael Truog <mjtruog@protonmail.com>
2023-06-20 18:14:27 -0700

    Version 2.0.6.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ReltoolUtil.Mixfile do
 
   def project do
     [app: :reltool_util,
-     version: "2.0.5",
+     version: "2.0.6",
      language: :erlang,
      erlc_options: [
        :deterministic,
